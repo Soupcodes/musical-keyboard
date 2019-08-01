546665fb06fad6ede953b170f73b5ca70e525c7c
Soupcodes <alantong789@gmail.com>
2019-08-01 09:48:16 +0100

    Sounds linked on click

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,128 +1,68 @@
 import React, { Component } from "react";
 import Key from "./Keys";
+import Akey from "../sound files/Anote.wav";
+import ASharp from "../sound files/ASharpnote.wav";
+import Bkey from "../sound files/Bnote.wav";
+import Ckey from "../sound files/Cnote.wav";
+import CSharp from "../sound files/CSharpnote.wav";
+import Dkey from "../sound files/Dnote.wav";
+import DSharp from "../sound files/DSharpnote.wav";
+import Ekey from "../sound files/Enote.wav";
+import Fkey from "../sound files/Fnote.wav";
+import FSharp from "../sound files/FSharpnote.wav";
+import Gkey from "../sound files/Gnote.wav";
+import GSharp from "../sound files/GSharpnote.wav";
 
 class Keyboard extends Component {
   state = {
     keys: [
       {
         key: "a",
-        note: (
-          <audio
-            data-key="65"
-            src="../sound files/Fnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="65" src={Fkey} type="audio/wav" />
       },
       {
         key: "s",
-        note: (
-          <audio
-            data-key="83"
-            src="../sound files/FSharpnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="83" src={FSharp} type="audio/wav" />
       },
       {
         key: "d",
-        note: (
-          <audio
-            data-key="68"
-            src="../sound files/Gnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="68" src={Gkey} type="audio/wav" />
       },
       {
         key: "f",
-        note: (
-          <audio
-            data-key="70"
-            src="../sound files/GSharpnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="70" src={GSharp} type="audio/wav" />
       },
       {
         key: "g",
-        note: (
-          <audio
-            data-key="71"
-            src="../sound files/Anote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="71" src={Akey} type="audio/wav" />
       },
       {
         key: "h",
-        note: (
-          <audio
-            data-key="72"
-            src="../sound files/ASharpnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="72" src={ASharp} type="audio/wav" />
       },
       {
         key: "j",
-        note: (
-          <audio
-            data-key="74"
-            src="../sound files/Bnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="74" src={Bkey} type="audio/wav" />
       },
       {
         key: "k",
-        note: (
-          <audio
-            data-key="75"
-            src="../sound files/Cnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="75" src={Ckey} type="audio/wav" />
       },
       {
         key: "l",
-        note: (
-          <audio
-            data-key="76"
-            src="../sound files/CSharpnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="76" src={CSharp} type="audio/wav" />
       },
       {
         key: ";",
-        note: (
-          <audio
-            data-key="59"
-            src="../sound files/Dnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="59" src={Dkey} type="audio/wav" />
       },
       {
         key: "'",
-        note: (
-          <audio
-            data-key="222"
-            src="../sound files/DSharpnote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="222" src={DSharp} type="audio/wav" />
       },
       {
         key: "#",
-        note: (
-          <audio
-            data-key="163"
-            src="../sound files/Enote.wav"
-            type="audio/wav"
-          />
-        )
+        note: <audio data-key="163" src={Ekey} type="audio/wav" />
       }
     ],
     playing: false
